9afb813922f582fa843b7a313e801f0625bd69a4
christian <christian.collosi@gmail.com>
2019-09-09 13:29:46 -0700

    Create new query for images that will be displayed in the Photos page using an aspect ratio of 16:9

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout fullMenu>
+    <section id="wrapper">
+      <header>
+        <div className="inner">
+          <h2>About Me</h2>
+          <p>Stuff about me</p>
+        </div>
+      </header>
+    </section>
+  </Layout>
+);
+
+export default IndexPage;
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+
+import Img from 'gatsby-image';
 
 import Layout from '../components/Layout';
 
-const IndexPage = () => (
-  <Layout fullMenu>
-    <section id="wrapper">
-      <header>
-        <div className="inner">
-          <h2>Photos</h2>
-          <p>My Pictures</p>
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulAsset(filter: { title: { glob: "_*" } }) {
+        edges {
+          node {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const images = data.allContentfulAsset.edges.map(edge => (
+    <div className="col-3-xlarge col-4-large col-6-medium">
+      <span className="image fit">
+        <Img sizes={{ ...edge.node.fluid, aspectRatio: 16 / 9 }} />
+      </span>
+    </div>
+  ));
+
+  return (
+    <Layout fullMenu>
+      <section id="wrapper">
+        <header>
+          <div className="inner">
+            <h2>Photos</h2>
+            <p>My photography portfolio</p>
+          </div>
+        </header>
+        <div className="wrapper">
+          <div className="inner">
+            <div className="box alt">
+              <div className="row gtr-uniform">{images}</div>
+            </div>
+          </div>
         </div>
-      </header>
-    </section>
-  </Layout>
-);
+      </section>
+    </Layout>
+  );
+};
 
 export default IndexPage;
